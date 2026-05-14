d04898a77af489228846edb13dd6cc6110946103
igustavo11 <gustavobrun1224@gmail.com>
2026-05-14 12:19:19 -0300

    feat: add Footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import { useTranslations } from 'next-intl'
+
+export default function Footer() {
+  const t = useTranslations('footer')
+
+  return (
+    <footer className="bg-[#dadada] py-8 text-center">
+      <p className="text-[#212121] text-2xl font-medium">
+        {t('copyright')}
+      </p>
+    </footer>
+  )
+}
