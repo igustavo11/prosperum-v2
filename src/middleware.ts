e1e805eb6af2f9987cfa92f36c3cfcc842ced1a0
igustavo11 <gustavobrun1224@gmail.com>
2026-05-14 11:28:43 -0300

    feat: add next-intl routing middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import createMiddleware from 'next-intl/middleware'
+import { defineRouting } from 'next-intl/routing'
+
+export const routing = defineRouting({
+  locales: ['pt', 'en'],
+  defaultLocale: 'pt',
+  localeDetection: true,
+})
+
+export default createMiddleware(routing)
+
+export const config = {
+  matcher: ['/((?!_next|_vercel|.*\\..*).*)'],
+}
