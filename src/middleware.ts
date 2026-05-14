2810d382b49de22f994f4c9bc9b241259833485d
igustavo11 <gustavobrun1224@gmail.com>
2026-05-14 11:29:30 -0300

    fix: set default locale to pt in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
-import createMiddleware from 'next-intl/middleware'
-import { defineRouting } from 'next-intl/routing'
+import createMiddleware from "next-intl/middleware";
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ['pt', 'en'],
-  defaultLocale: 'pt',
+  locales: ["pt", "en"],
+  defaultLocale: "pt",
   localeDetection: true,
-})
+});
 
-export default createMiddleware(routing)
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!_next|_vercel|.*\\..*).*)'],
-}
+  matcher: ["/((?!_next|_vercel|.*\\..*).*)"],
+};
